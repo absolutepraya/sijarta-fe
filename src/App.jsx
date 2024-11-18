c18abee72d31eed5877af4b08ae2e072197eba2f
Daffa Abhipraya <daffaabhiprayaputra@gmail.com>
2024-11-18 20:21:56 +0700

    MyPay done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
 import LandingPage from './pages/green/LandingPage';
 import LoginPage from './pages/green/LoginPage';
@@ -10,24 +10,68 @@ import Homepage from './pages/green/Homepage';
 import SubkategoriPengguna from './pages/green/SubkategoriPengguna';
 import SubkategoriPekerja from './pages/green/SubkategoriPekerja';
 import ViewPekerja from './pages/green/ViewPekerja';
+import MyPay from './pages/red/MyPay';
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path='/*' element={<Navigate to='/' replace />} />
-        <Route index element={<LandingPage />} />
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/register' element={<RegisterPage />} />
-        <Route path='/register/pengguna' element={<RegisterPengguna />} />
-        <Route path='/register/pekerja' element={<RegisterPekerja />} />
-        <Route path='/home' element={<Homepage />} />
-        <Route path='/subkategori-pengguna' element={<SubkategoriPengguna />} />
-        <Route path='/subkategori-pekerja' element={<SubkategoriPekerja />} />
-        <Route path='/view-pekerja' element={<ViewPekerja />} />
-      </Routes>
-    </Router>
-  )
+	return (
+		<Router>
+			<Routes>
+				<Route
+					path="/*"
+					element={
+						<Navigate
+							to="/"
+							replace
+						/>
+					}
+				/>
+				<Route
+					index
+					element={<LandingPage />}
+				/>
+				<Route
+					path="/login"
+					element={<LoginPage />}
+				/>
+				<Route
+					path="/register"
+					element={<RegisterPage />}
+				/>
+				<Route
+					path="/register/pengguna"
+					element={<RegisterPengguna />}
+				/>
+				<Route
+					path="/register/pekerja"
+					element={<RegisterPekerja />}
+				/>
+				<Route
+					path="/home"
+					element={<Homepage />}
+				/>
+				<Route
+					path="/subkategori-pengguna"
+					element={<SubkategoriPengguna />}
+				/>
+				<Route
+					path="/subkategori-pekerja"
+					element={<SubkategoriPekerja />}
+				/>
+				<Route
+					path="/view-pekerja"
+					element={<ViewPekerja />}
+				/>
+				<Route
+					path="/mypay/pengguna"
+					element={<MyPay isPekerja={false} />}
+				/>
+				<Route
+					path="/mypay/pekerja"
+					element={<MyPay isPekerja={true} />}
+				/>
+			</Routes>
+		</Router>
+	);
 }
 
-export default App
+export default App;
