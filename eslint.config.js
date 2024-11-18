6f3da6f2269b4b39af35e17b64f5014752831815
githubhiseka <susantoabhiseka@gmail.com>
2024-11-18 10:24:45 +0700

    some changes to eslint (kocak anjg)

--- eslint.config.js
+++ eslint.config.js
@@ -2,11 +2,20 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
 
+/** @type {import('eslint').Linter.Config} */
+export default {
+  files: ["**/*.{js,mjs,cjs,jsx}"],
 
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
-];
+  languageOptions: {
+    globals: globals.browser,
+  },
+
+  plugins: {
+    react: pluginReact,
+  },
+
+  extends: [
+    pluginJs.configs.recommended,
+    pluginReact.configs.flat.recommended,
+  ],
+};
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -3,13 +3,15 @@ export default function Homepage() {
     <div className="flex h-screen w-screen items-center justify-center bg-gray-300">
       <div className="flex w-3/4 flex-col items-center justify-center gap-8 rounded-lg bg-white py-8 shadow-lg">
         {/* header div */}
-        <div className="flex flex-row w-[90%] justify-between gap-6">
+        <div className="flex w-[90%] flex-row justify-between gap-6">
           {/* kategori dropdown */}
           <select className="w-1/5">
-            <option value="" disabled selected>Pilih kategori</option>
+            <option value="" disabled selected>
+              Pilih kategori
+            </option>
           </select>
           {/* nama subkategori */}
-          <h1 className="w-1/5 text-2xl font-bold text-center">Subkategori</h1>
+          <h1 className="w-1/5 text-center text-2xl font-bold">Subkategori</h1>
           {/* search bar */}
           <input
             type="text"
@@ -19,9 +21,9 @@ export default function Homepage() {
         </div>
 
         {/* content div */}
-        <div className="w-[90%] flex flex-col items-center">
+        <div className="flex w-[90%] flex-col items-center">
           {/* div kategori */}
-          <div className="w-full border-2 border-black rounded-md p-2">
+          <div className="w-full rounded-md border-2 border-black p-2">
             <h1 className="text-lg">Kategori jasa 1</h1>
           </div>
           {/* div subkategori */}
