d4704e3ea2be2c14ef4ddcb02bdb632ea7f266a7
githubhiseka <susantoabhiseka@gmail.com>
2024-12-09 23:37:53 +0700

    push viewpekerja (static doang gaperlu dipeduliin)

--- src/pages/green/ViewPekerja.jsx
+++ src/pages/green/ViewPekerja.jsx
@@ -1,16 +1,23 @@
+import NavBar from "../../components/NavBar"
+
 export default function ViewPekerja() {
   return (
     <div className="flex h-screen w-screen items-center justify-center bg-gray-300">
+      <NavBar
+				isLoggedIn={true}
+				role="Pengguna"
+				name="John Doe"
+			/>
       <div className="flex w-2/5 flex-col items-center justify-center gap-8 rounded-lg bg-white py-8 shadow-lg">
         <h1 className="text-2xl font-bold">PROFIL PEKERJA</h1>
         <img src="meme"/>
         <div className="w-[90%] flex flex-col gap-2">
-          <p>Nama: abhiseka</p>
-          <p>Rating: 9/10</p>
-          <p>Jumlah pesanan selesai: 20</p>
-          <p>No. HP: 08123456789</p>
-          <p>Tanggal lahir: 06/01/2006</p>
-          <p>Alamat: Jl. Ambasing</p>
+          <p>Nama: User 7</p>
+          <p>Rating: 4.5/5</p>
+          <p>Jumlah pesanan selesai: 50</p>
+          <p>No. HP: 081234567896</p>
+          <p>Tanggal lahir: 2002-07-07</p>
+          <p>Alamat: Alamat 7</p>
         </div>
       </div>
     </div>
